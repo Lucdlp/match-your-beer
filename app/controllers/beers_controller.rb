446810ce213e249e66afd2e1c2f7bba0e37b60d9
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-06-16 21:25:56 +0200

    mapfinishsoon

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -13,14 +13,20 @@ class BeersController < ApplicationController
     @beer = Beer.find(params[:id])
     @shops = Shop.all
     @review = Review.new
-
-    @shops = Shop.all
+    if user_signed_in?
+      @shops = Shop.near([current_user.latitude, current_user.longitude], 50, units: :km)
+      @shops = Shop.all if @shops.empty?
+    else
+      @shops = Shop.all
+    end
 
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @shops.geocoded.map do |shop|
       {
         lat: shop.latitude,
-        lng: shop.longitude
+        lng: shop.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { shop: shop }),
+        image_url: helpers.asset_url("beer.png")
       }
     end
   end
