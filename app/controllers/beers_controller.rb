447035ec56fbdab409076a75187604a870e2e6c0
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-06-21 20:38:25 +0200

    mapbox-sol-bug

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -17,24 +17,31 @@ class BeersController < ApplicationController
     if user_signed_in?
       @shops = Shop.near([current_user.latitude, current_user.longitude], 50, units: :km)
       @shops = Shop.all if @shops.empty?
+      @markers = @shops.geocoded.map do |shop|
+        {
+          lat: shop.latitude,
+          lng: shop.longitude,
+          info_window: render_to_string(partial: "info_window_shop", locals: { shop: shop }),
+          image_url: helpers.asset_url("beer.png")
+        }
+      end
+        user_marker = {
+          lat: current_user.latitude,
+          lng: current_user.longitude,
+          info_window: render_to_string(partial: "info_window_user", locals: { current_user: current_user }),
+          image_url: helpers.asset_url("homme.png")
+        }
+      @markers << user_marker
     else
       @shops = Shop.all
+      @markers = @shops.geocoded.map do |shop|
+        {
+          lat: shop.latitude,
+          lng: shop.longitude,
+          info_window: render_to_string(partial: "info_window_shop", locals: { shop: shop }),
+          image_url: helpers.asset_url("beer.png")
+        }
+      end
     end
-    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    @markers = @shops.geocoded.map do |shop|
-      {
-        lat: shop.latitude,
-        lng: shop.longitude,
-        info_window: render_to_string(partial: "info_window_shop", locals: { shop: shop }),
-        image_url: helpers.asset_url("beer.png")
-      }
-    end
-    user_marker = {
-      lat: current_user.latitude,
-      lng: current_user.longitude,
-      info_window: render_to_string(partial: "info_window_user", locals: { current_user: current_user }),
-      image_url: helpers.asset_url("homme.png")
-    }
-    @markers = @markers << user_marker
   end
 end
