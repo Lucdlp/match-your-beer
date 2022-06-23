0242e044e2ff6d6570db359989e4ee9d07778c10
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-06-23 19:33:55 +0200

    ajouter sur devise les caracteristiques de user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_address, :first_name, :surname, :age, :gender])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_address, :first_name, :surname, :age, :gender])
   end
 end
